2b5162762de46b438e6aa196e2f0e7cecbccfb5c
jkangasluoma <jarkko.kangasluoma@almamedia.fi>
2021-05-04 19:02:18 +0300

    Feat - misc updates from common-env.

---
+++ infra/lib/utils/policy-aspect/ResourceRemovalPolicyTesterAspect.ts
@@ -0,0 +1,35 @@
+import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import { Ec } from '@almamedia/cdk-accounts-and-environments';
+import { Annotations } from '@aws-cdk/core';
+
+/**
+ * Bucket removal policy verifier/tester checks that stable environments have
+ * RETAIN policies on buckets.
+ */
+export class ResourceRemovalPolicyTesterAspect implements cdk.IAspect {
+    public visit(node: cdk.IConstruct): void {
+        if (node instanceof s3.Bucket || node instanceof dynamodb.Table) {
+            const cfn = node.node.defaultChild as cdk.CfnResource;
+            if (Ec.isStable(node)) {
+                if (
+                    !cfn.cfnOptions.deletionPolicy ||
+                    ![cdk.CfnDeletionPolicy.SNAPSHOT, cdk.CfnDeletionPolicy.RETAIN].includes(
+                        cfn.cfnOptions.deletionPolicy,
+                    )
+                ) {
+                    Annotations.of(node).addError(
+                        `Resource '${node.node.id}' (S3/Dynamodb) removal policy should be RETAIN or SNAPSHOT in stable envs.`,
+                    );
+                }
+            } else {
+                if (!cfn.cfnOptions.deletionPolicy || cfn.cfnOptions.deletionPolicy !== cdk.CfnDeletionPolicy.DELETE) {
+                    Annotations.of(node).addError(
+                        `Make sure to define a removal policy for resource '${node.node.id}' (S3/Dynamodb) in non-stable envs.`,
+                    );
+                }
+            }
+        }
+    }
+}
